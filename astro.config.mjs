1207c35a5447bc0394a0bb8eabbd1848ba30bdc7
Lena EUGENE <lenaeugene@gmail.com>
2023-06-13 09:49:18 +0200

    Markdoc is added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()],
-  })
+  integrations: [react(), markdoc()]
+});
